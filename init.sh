23e69263b8dfff50d775510204bc77f3d57ac3bc
liyueke <liyueke@huobi.com>
2017-07-25 13:59:23 +0800

    add start

--- init.sh
+++ init.sh
@@ -1,7 +1,10 @@
 #geth --datadir "./" init genesis.json
-geth --datadir "./"  --nodiscover  2>>geth.log
+geth --datadir "./"  --nodiscover  2>>geth.log &
 
-geth attach http://127.0.0.1:8545 << EOF
+geth attach  ./geth.ipc << EOF
 personal.newAccount("123")
 miner.start(1)
-EOF%
+
+EO%
+
+killall geth
---
+++ start.sh
@@ -0,0 +1,2 @@
+geth --datadir "./"  --nodiscover console --rpcaddr=127.0.0.1 --rpcport=8545  2>>geth.log
+
