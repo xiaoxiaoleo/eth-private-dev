f7b9bc1998b6360c1eb7ccfbe79c3cca4c602ecd
liyueke <liyueke@huobi.com>
2017-07-25 14:14:10 +0800

    add rpc

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-geth --datadir "./"  --nodiscover console --rpcaddr=127.0.0.1 --rpcport=8545  2>>geth.log
+geth --datadir "./"  --nodiscover  --rpc --rpcaddr "127.0.0.1" --rpcport "8545"  console   2>>geth.log
 
