0890289b9960a5ab66da0ebe801d30cc4ffdfaca
liyueke <liyueke@huobi.com>
2017-07-25 14:02:46 +0800

    fix bugs

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #geth --datadir "./" init genesis.json
-geth --datadir "./"  --nodiscover  2>>geth.log &
+geth --datadir "./" --rpcaddr=127.0.0.1 --rpcport=8545 --nodiscover  2>>geth.log &
 
 geth attach  ./geth.ipc << EOF
 personal.newAccount("123")
 miner.start(1)
 
-EO%
+EOF
 
 killall geth
