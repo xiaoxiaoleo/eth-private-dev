7cf6eb960ac3d24ab9bf145fe2ed4c9486d54d18
小小leo <youfbi@foxmail.com>
2017-08-05 16:06:24 +0800

    update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-geth --datadir "./" init genesis.json
-geth --datadir "./"  --rpc --rpcaddr=127.0.0.1 --rpcport=8545 --nodiscover  2>>geth.log &
+geth --datadir "./data" init genesis.json
+geth --datadir "./data"  --rpc --rpcaddr=127.0.0.1 --rpcport=8545 --nodiscover  2>>geth.log &
 
 geth attach  ./geth.ipc << EOF
 personal.newAccount("123")
